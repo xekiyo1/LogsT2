85a8edf983a8f63ccddba9f6117e736ef4388604
Hernan Poblete <hernan.poblete@ug.uchile.cl>
2026-06-14 20:00:19 -0400

    fix: Adds recursiveness to balancing function

--- src/arbolitos/AVL.cpp
+++ src/arbolitos/AVL.cpp
@@ -16,14 +16,22 @@ Nodo* AVL::insert(uint val){
 }
 
 void AVL::balance(Nodo* node){
+    if (node ==nullptr) return;
     int bf = getBalanceFactor(node); //How do i get a bf? >~<
+
+    Nodo* parent = node->parent;
     Nodo* child;
 
     if(bf>=2){
+
+        // If the balance is disturbed left-wise, takes the right child
         child = node->der;
 
+        // Then, searches for the biggest child. If balance factor is less than 0
+        // It means the disturber is to the left, so a rotation is necessary
         if(getBalanceFactor(child)<0) zig(child);
 
+        //Rotates
         zag(node);
     }else if(bf<=-2){
         child = node->izq;
@@ -33,6 +41,8 @@ void AVL::balance(Nodo* node){
 
         zig(node);
     }
+
+    balance(parent);
 
 }
 
