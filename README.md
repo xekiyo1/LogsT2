9f41d79311cee01b3c31abfb5d8e3d7e2d1cc8a7
ElNChou <ignacio.azocar.carcamo@gmail.com>
2026-06-15 10:55:19 -0400

    README and tests legibles

--- README.md
+++ README.md
@@ -1,5 +1,40 @@
-# LogsT2
-logaratimios
+# Tarea 2 Diseño y Análisis de Algoritmos: árboles AVL y "SplayTree"-s
+
+El presente programa ejecuta pruebas de rendimiento para dos algoritmos de árboles de búsqueda binaria.
+El objetivo del proyecto es la comparación en tiempo de construcción y de búsqueda de ambos algoritmos para una serie de números
+aleatorios, ya sean a elección uniforme o sesgada, con tal de encontrar los casos en donde cada árbol funciona mejor.
+
+
+## Requisitos
+
+- Se requiere un sistema operativo basado en Unix que cuente con los siguientes comandos/librerias
+    + grep
+    + ls
+    + rm
+    + gcc (version >= 11.4)
+    + xargs (version >= 4.8.0)
+    + chmod
+    + make
+    + test
+
+## Correr el programa
+Para correr la tarea, basta con ir al directorio desde una terminal y correr
+
+```bash
+make run 
+```
+
+Esto creará diversos archivos .csv con las métricas obtenidas de la creación de los árboles,
+mientras imprime en tiempo real la información que va rescatando. Todos los archivos aparecerán en la carpeta donde se esté corriendo la tarea.
+
+
+El código está difícil de leer (principalmente en las rotaciones) por lo que si se necesita ayuda de confianza
+en la implementación, se hicieron tests en src/main/test.cpp. Para correrlos, usar:
+
+``` bash
+make test
+```
+
 ```text
 ⣿⣿⣿⣿⣿⣿⠟⠁⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⣿⣿⣿⣿
 ⣿⣿⣿⣿⣿⡏⠂⠂⠂⠂⢙⠛⠙⠛⠻⢿⠟⠁⠂⠂⠸⣿⣿⣿
@@ -12,3 +47,4 @@ logaratimios
 ⣿⡌⢻⣿⣿⣿⣿⣿⣿⣿⣿⣍⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢁⣾
 ⣿⡘⠦⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⣡⣾⣿
 ```
+^ imagen increíble! (es apoyo moral, no es relevante a la ejecución)
--- src/main/test.cpp
+++ src/main/test.cpp
@@ -2,27 +2,46 @@
 // Created by elnchou on 14-06-26.
 //
 #include <assert.h>
-
 #include "../arbolitos/SplayTree.h"
+#include <iostream>
+#include <vector>
 
-SplayTree hola;
-void insert(uint i) {
-    hola.insert(i);
-    hola.print();
+SplayTree arbolito;
+/**
+ * Comprueba que el SplayTree haya realizado las rotaciones correctas para quedar en una estructura
+ * específica, a base de crear un BTree con inserciones distintas.
+ * @param valores Valores que serán insertados en un BTree normal, en orden.
+ */
+void check(std::vector<uint> valores) {
+    BTree comprobador;
+    for (uint x : valores) comprobador.insert(x);
+    arbolito.print();
+    comprobador.print();
+
+    assert(arbolito == comprobador);
+
+    comprobador.clear();
 }
 
+/// Utilizamos este programa para comprobar el funcionamiento de los zig y zags. Veamos cómo va...
 int main(){
     //para probar que funcionan bien el zig y el zag
+    arbolito.insert(2);
+    arbolito.insert(3); //2(,3) -> zag -> 3(2,)
+    check({3,2});
+    arbolito.clear();
 
-    insert(5);
-    insert(2); //2(,5)
-    insert(7); //2(,5(,7)) -> 7(5(2,),)
-    insert(20);
-    insert(8);
-    insert(8);
-    insert(9);
-    insert(10);
+    arbolito.insert(6);
+    arbolito.insert(2); //2(,6)
+    check({2,6});
+    //ahora si inserto algo entre 2 y 6, tendré que hacer un zag-zig
+    arbolito.insert(4); //2(,6(4,)) -> zagzig -> 4(2(),6())
 
-    hola.search(10);
-    hola.search(11);
+    check({4,2,6});
+
+    //ahora, si inserto algo mayor a 2, tendré que hacer zig-zag
+    arbolito.insert(3); // 4(2(,3),6(,)) -> zig-zag -> 3(2,4(,6))
+    check({3,2,4,6});
+
+    std::cout << "Todo funcionó!" << std::endl;
 }
